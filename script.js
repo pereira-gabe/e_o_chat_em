140f196a8d4e260c8677a848aa71a1172ced3815
Gabriel Pereira <hadriangabriel.trsp@gmail.com>
2025-04-07 16:15:48 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let contador = parseInt(document.getElementById("contadorGPT").innerText) || 0;
-
-function aumentar(){
-    contador++;
-    document.getElementById("contadorGPT").innerText = contador;
-}
